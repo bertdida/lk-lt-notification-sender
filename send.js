da8ff75eacf43730985672a26849f472c2afd27e
bertdida <38489033+bertdida@users.noreply.github.com>
2021-06-07 17:30:15 +0800

    Catch error

--- send.js
+++ send.js
@@ -7,6 +7,10 @@ const filename = process.argv[2];
 fs.readFile(filename, "utf8", async function (_, data) {
   const { HOST, PORT = 5000 } = process.env;
   const endpoint = `http://${HOST}:${PORT}/api/notification`;
-  axios.post(endpoint, JSON.parse(data));
+
+  try {
+    await axios.post(endpoint, JSON.parse(data));
+  } catch (error) {}
+
   fs.unlink(filename, () => {});
 });
